39fa78714f93c7adc4ee4bdcfbb60931774811c7
billyjitsu <82428978+billyjitsu@users.noreply.github.com>
2024-11-16 09:18:58 +0700

    add tokens, create factory for sig deployments

---
+++ FlashFi-Contracts/packages/hardhat/contracts/Token.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Token is ERC20, Ownable {
+    constructor() ERC20("Token", "TKN"){
+        _mint(msg.sender, 10000000 * 10 ** 18); // 10 million tokens with 18 decimals
+    }
+
+    function mint() public {
+        _mint(msg.sender, 10000000 * 10 ** 18);
+    }
+
+    function getAddress() public view returns (address) {
+        return address(this);
+    }
+}
